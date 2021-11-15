4f08e785ae0c8b8442440081ec045e0cafee77b5
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2021-11-15 14:15:07 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,7 +1,8 @@
 use mimalloc::MiMalloc;
 
+/// Memory allocator ( MiMalloc ).
 #[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+static MEMALLOC: MiMalloc = MiMalloc;
 
 use axum::{
     extract::{Extension, Form, Multipart, Path, Query},
@@ -12,18 +13,16 @@ use axum::{
 use tower::ServiceBuilder;
 use tower_cookies::{CookieManagerLayer, Cookies};
 
-use tokio::sync::{
-    mpsc::{channel, Receiver, Sender},
-    Mutex,
-};
+use tokio::sync::{mpsc, oneshot};
 
 use database::{
     genquery::{GenQuery, Part},
     Database,
 };
 
-use std::{collections::HashMap, sync::Arc, thread};
+use std::{collections::HashMap, thread};
 
+/// Query to be sent to server thread, implements IntoResponse.
 struct ServerQuery {
     pub x: Box<GenQuery>,
 }
@@ -36,15 +35,16 @@ impl ServerQuery {
     }
 }
 
-struct SharedState {
-    tx: Sender<ServerQuery>,
-    rx: Receiver<ServerQuery>,
+/// Message to server thread, includes oneshot Sender for reply.
+struct ServerMessage {
+    pub sq: ServerQuery,
+    pub tx: oneshot::Sender<ServerQuery>,
 }
 
+/// Main function ( execution starts here ).
 #[tokio::main]
 async fn main() {
-    let (tx, mut server_rx): (Sender<ServerQuery>, Receiver<ServerQuery>) = channel(1);
-    let (server_tx, rx): (Sender<ServerQuery>, Receiver<ServerQuery>) = channel(1);
+    let (tx, mut rx) = mpsc::channel::<ServerMessage>(1);
 
     // This is the server thread (synchronous).
     thread::spawn(move || {
@@ -53,24 +53,19 @@ async fn main() {
         ));
         let db = Database::new(stg, database::init::INITSQL);
         loop {
-            let mut q = server_rx.blocking_recv().unwrap();
-            let sql = "EXEC web.Main()";
-            db.run_timed(sql, &mut *q.x);
-            let _x = server_tx.blocking_send(q);
+            let mut sm = rx.blocking_recv().unwrap();
+            db.run_timed("EXEC web.Main()", &mut *sm.sq.x);
+            let _x = sm.tx.send(sm.sq);
             db.save();
         }
     });
 
-    let state = Arc::new(Mutex::new(SharedState { tx, rx }));
-
     // build our application with a single route
-    let app = Router::new()
-        .route("/*key", get(my_get_handler).post(my_post_handler))
-        .layer(
-            ServiceBuilder::new()
-                .layer(CookieManagerLayer::new())
-                .layer(AddExtensionLayer::new(state)),
-        );
+    let app = Router::new().route("/*key", get(h_get).post(h_post)).layer(
+        ServiceBuilder::new()
+            .layer(CookieManagerLayer::new())
+            .layer(AddExtensionLayer::new(tx)),
+    );
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -79,50 +74,19 @@ async fn main() {
         .unwrap();
 }
 
-async fn my_get_handler(
-    Extension(state): Extension<Arc<Mutex<SharedState>>>,
-    Path(path): Path<String>,
-    Query(params): Query<HashMap<String, String>>,
-    tcookies: Cookies,
-) -> ServerQuery {
-    
-    // Get cookies into a HashMap.
-    let mut cookies = HashMap::new();
-    for cookie in tcookies.list() {
+/// Get HashMap of cookies from Cookies.
+fn map_cookies(cookies: Cookies) -> HashMap<String, String> {
+    let mut result = HashMap::new();
+    for cookie in cookies.list() {
         let (name, value) = cookie.name_value();
-        cookies.insert(name.to_string(), value.to_string());
+        result.insert(name.to_string(), value.to_string());
     }
-
-    // Build the ServerQuery.
-    let mut sq = ServerQuery::new();
-    sq.x.path = path;
-    sq.x.query = params;
-    sq.x.cookies = cookies;
-
-    // Send query to database thread ( and get it back ).
-    let mut state = state.lock().await;
-    let _x = state.tx.send(sq).await;
-    state.rx.recv().await.unwrap()
+    result
 }
 
-async fn my_post_handler(
-    Extension(state): Extension<Arc<Mutex<SharedState>>>,
-    Path(path): Path<String>,
-    Query(params): Query<HashMap<String, String>>,
-    form: Option<Form<HashMap<String, String>>>,
-    tcookies: Cookies,
-    mp: Option<Multipart>,
-) -> ServerQuery {
-
-    // Get the cookies into a HashMap.
-    let mut cookies = HashMap::new();
-    for cookie in tcookies.list() {
-        let (name, value) = cookie.name_value();
-        cookies.insert(name.to_string(), value.to_string());
-    }
-
-    // Get Vec of Parts.
-    let mut parts = Vec::new();
+/// Get Vec of Parts from MultiPart.
+async fn map_parts(mp: Option<Multipart>) -> Vec<Part> {
+    let mut result = Vec::new();
     if let Some(mut mp) = mp {
         while let Some(field) = mp.next_field().await.unwrap() {
             let name = field.name().unwrap().to_string();
@@ -143,7 +107,7 @@ async fn my_post_handler(
             } else if let Ok(bytes) = field.bytes().await {
                 data = bytes.to_vec()
             }
-            parts.push(Part {
+            result.push(Part {
                 name,
                 file_name,
                 content_type,
@@ -152,28 +116,57 @@ async fn my_post_handler(
             });
         }
     }
+    result
+}
 
+/// Handler for http GET requests.
+async fn h_get(
+    state: Extension<mpsc::Sender<ServerMessage>>,
+    path: Path<String>,
+    params: Query<HashMap<String, String>>,
+    cookies: Cookies,
+) -> ServerQuery {
     // Build the ServerQuery.
     let mut sq = ServerQuery::new();
-    sq.x.path = path;
-    sq.x.query = params;
-    sq.x.cookies = cookies;
-    sq.x.parts = parts;
+    sq.x.path = path.0;
+    sq.x.params = params.0;
+    sq.x.cookies = map_cookies(cookies);
+
+    // Send query to database thread ( and get it back ).
+    let (tx, rx) = oneshot::channel::<ServerQuery>();
+    let _err = state.send(ServerMessage { sq, tx }).await;
+    rx.await.unwrap()
+}
+
+/// Handler for http POST requests.
+async fn h_post(
+    state: Extension<mpsc::Sender<ServerMessage>>,
+    path: Path<String>,
+    params: Query<HashMap<String, String>>,
+    cookies: Cookies,
+    form: Option<Form<HashMap<String, String>>>,
+    multipart: Option<Multipart>,
+) -> ServerQuery {
+    // Build the ServerQuery.
+    let mut sq = ServerQuery::new();
+    sq.x.path = path.0;
+    sq.x.params = params.0;
+    sq.x.cookies = map_cookies(cookies);
     if let Some(Form(form)) = form {
         sq.x.form = form;
+    } else {
+        sq.x.parts = map_parts(multipart).await;
     }
 
-    // Send the ServerQuery to database thread ( and get it back ).
-    let mut state = state.lock().await;
-    let _x = state.tx.send(sq).await;
-    state.rx.recv().await.unwrap()
+    // Send query to database thread ( and get it back ).
+    let (tx, rx) = oneshot::channel::<ServerQuery>();
+    let _err = state.send(ServerMessage { sq, tx }).await;
+    rx.await.unwrap()
 }
 
 use axum::{
     body::{Bytes, Full},
-    http::header::HeaderName,
-    http::status::StatusCode,
-    http::{HeaderValue, Response},
+    http::{header::HeaderName, status::StatusCode, HeaderValue, Response},
     response::IntoResponse,
 };
 
