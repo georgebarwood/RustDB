50288cdd98f3f2a39edabdd86e05343c3a949e62
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2021-12-01 19:33:47 +0000

    Add files via upload

--- examples/test.rs
+++ examples/test.rs
@@ -1,31 +1,39 @@
-use rustdb::{Database, SharedPagedData, SimpleFileStorage, WebTransaction, INITSQL};
+use rustdb::{Database, SharedPagedData, WebTransaction, INITSQL};
 use std::net::TcpListener;
 use std::sync::Arc;
 
 fn main() {
-    let sfs = Box::new(SimpleFileStorage::new(
+    rustdb::atomfile::test();
+
+    // let stg = Box::new(rustdb::SimpleFileStorage::new(
+    let stg = Box::new(rustdb::AtomicFile::new(
         "c:\\Users\\pc\\rust\\sftest01.rustdb",
     ));
-    let spd = Arc::new(SharedPagedData::new(sfs));
+    let spd = Arc::new(SharedPagedData::new(stg));
     let apd = spd.open_write();
     let db = Database::new(apd, INITSQL);
 
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     for tcps in listener.incoming() {
-        if let Ok(mut tcps) = tcps {
-            match WebTransaction::new(&tcps) {
-                Ok(mut wq) => {
-                    wq.trace();
-                    let sql = "EXEC web.Main()";
-                    // Execute SQL. http response, SQL output, (status,headers,content) is accumulated in wq.
-                    db.run_timed(&sql, &mut wq);
-                    // Write the http response to the TCP stream.
-                    let _err = wq.write(&mut tcps);
-                    // Save database changes to disk.
-                    db.save();
-                }
-                Err(e) => {
-                    println!("Error getting query {:?}", e);
+        match tcps {
+            Err(e) => {
+                println!("Incoming connection error {:?}", e);
+            }
+            Ok(mut tcps) => {
+                match WebTransaction::new(&tcps) {
+                    Err(e) => {
+                        println!("Error getting query {:?}", e);
+                    }
+                    Ok(mut wq) => {
+                        wq.trace();
+                        let sql = "EXEC web.Main()";
+                        // Execute SQL. http response, SQL output, (status,headers,content) is accumulated in wq.
+                        db.run_timed(sql, &mut wq);
+                        // Write the http response to the TCP stream.
+                        let _err = wq.write(&mut tcps);
+                        // Save database changes to disk.
+                        db.save();
+                    }
                 }
             }
         }
