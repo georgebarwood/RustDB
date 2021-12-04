acdd8c18bd8edfaf1a2d1f4c9507e5e70565dd9a
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2021-12-04 11:39:17 +0000

    Add files via upload

--- src/compact.rs
+++ src/compact.rs
@@ -142,7 +142,8 @@ impl CompactFile {
             while old_ext > ext {
                 old_ext -= 1;
                 let fp = util::getu64(&info, 2 + old_ext * 8);
-                self.ep_free.insert(fp);
+                info.resize(info.len() - 8, 0); // Important or info could over-write data later.  
+                self.ep_free.insert(fp);                              
             }
             // Allocate new pages.
             while old_ext < ext {
@@ -169,6 +170,7 @@ impl CompactFile {
         debug_assert!(done == size);
 
         // Write the info.
+        debug_assert!( info.len() == 2 + ext * 8 );
         self.stg.write_vec(foff, info);
     }
 
